a275e9cf6a21059a0b915b3d5c4c5dd6de88c25f
shirley <shirley@triggybet.com>
2019-12-26 13:02:02 +0100

    install eslint and override some rules

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 // src/index
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 function Root() {
     return <h1>Hello, World.</h1>;
 }
 
-ReactDOM.render(<Root />, document.getElementById("root"));
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 // import "core-js/stable";
 
